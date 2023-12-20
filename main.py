481b7a24bec72aaefc12870a2ecff634f859de69
etiti <christopherobinna27@gmail.com>
2023-12-20 01:10:12 +0100

    first commit

--- main.py
+++ main.py
@@ -1,58 +1,39 @@
-import turtle
-
-import colorgram
 import random
-
+import turtle
 from turtle import Turtle
 
-turtle.colormode(255)
 
-tim = Turtle()
-# extracted_color=colorgram.extract('hirst.jpg', 30)
-# rgb_color=[]
-# for color in extracted_color:
-#     r=color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#
-#     new_color=(r,g,b)
-#     rgb_color.append(new_color)
-# print(rgb_color)
-color_list = [(241, 242, 47), (177, 173, 8), (239, 57, 213), (36, 180, 10), (158, 4, 90), (235, 48, 10),
-              (212, 8, 4), (59, 145, 241), (67, 4, 146), (89, 239, 248), (244, 53, 17), (221, 138, 63),
-              (234, 214, 2), (118, 232, 208), (216, 121, 200), (172, 52, 108), (60, 133, 224), (43, 157, 24),
-              (119, 196, 98), (239, 161, 220), (52, 7, 108),(243, 167, 155), (251, 6, 10), (96, 77, 149),
+screen=turtle.Screen()
+prediction = screen.textinput(title='Predict', prompt="who will win this race? 'red', 'blue', 'green', 'grey', 'brown'or 'black' ")
 
-              (251, 10, 7), (96, 7, 58)]
-tim.penup()
-tim.setheading(220)
-tim.forward(300)
+colors=['red', 'blue', 'green', 'grey', 'brown','black']
+timmy_position=[-40, -10, 20, 50, 80,110]
+new_turtle=[]
+is_race_on=False
+for index in range(0, len(colors)):
+    timmy = Turtle(shape='turtle')
+    timmy.penup()
+    timmy.goto(x=-270, y=timmy_position[index])
+    timmy.color(colors[index])
+    new_turtle.append(timmy)
 
-def create_dot():
-    tim.setheading(0)
-    for _ in range(10):
-        tim.dot(20, random.choice(color_list), )
-        tim.forward(50)
+if prediction:
+    is_race_on=True
+while is_race_on:
 
-def moveback():
-    tim.left(90)
-    tim.forward(30)
-    tim.left(90)
-    tim.forward(500)
-    tim.setheading(0)
-for _ in range(10):
-    create_dot()
-    moveback()
-tim.hideturtle()
+    for tim in new_turtle:
+        movement = random.randint(0, 10)
+        tim.forward(movement)
+        if tim.xcor() >270:
+            is_race_on=False
+            winning_color=tim.pencolor()
+            if winning_color == prediction.lower():
+                print(f'you won!, the winning turtle is "{winning_color}" turtle')
+            else:
+                print(f'you lost!, the winning turtle is "{winning_color}" turtle')
 
 
-# def colors():
-#     r = random.randint(0, 255)
-#     g = random.randint(0, 255)
-#     b = random.randint(0, 255)
-#     new_color = (r,g,b)
-#     return new_color
+screen.setup(height=500, width=600)
+screen.exitonclick()
 
 
-screen = turtle.Screen()
-turtle.exitonclick()
